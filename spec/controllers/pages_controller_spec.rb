7c6c7b19f4b780417216d59206d7d9d0d5ed1901
Phil Rand <philrand@gmail.com>
2010-09-09 21:57:09 -0700

    DRY enforcement on the pages spec.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'help'" do
     it "should be successful" do
@@ -10,7 +14,8 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'help'
-      response.should have_selector("title", :content => " | Help")
+      response.should have_selector("title", 
+                                    :content => @base_title + " | Help")
     end
   end
   
@@ -21,7 +26,8 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => " | Home")
+      response.should have_selector("title", 
+                                    :content => @base_title + " | Home")
     end
   end
   
@@ -32,10 +38,9 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", 
-        :content => "Ruby on Rails Tutorial Sample App | Contact")
+        response.should have_selector("title", 
+                                      :content => @base_title + " | Contact")
     end
-    
   end
   
   describe "GET 'about'" do
@@ -45,9 +50,8 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => " | About")
+      response.should have_selector("title", 
+                                    :content => @base_title + " | About")
     end
   end
-  
-
 end
