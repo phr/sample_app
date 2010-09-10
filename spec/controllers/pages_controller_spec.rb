77c34ee665db938891ce708c20670fb164921247
Phil Rand <philrand@gmail.com>
2010-09-09 21:37:31 -0700

    Finished help page.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,12 +3,23 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => " | Help")
+    end
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-    it "should should have the right title" do
+    it "should have the right title" do
       get 'home'
       response.should have_selector("title", :content => " | Home")
     end
@@ -19,7 +30,7 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
-    it "should should have the right title" do
+    it "should have the right title" do
       get 'contact'
       response.should have_selector("title", 
         :content => "Ruby on Rails Tutorial Sample App | Contact")
@@ -32,7 +43,7 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
-    it "should should have the right title" do
+    it "should have the right title" do
       get 'about'
       response.should have_selector("title", :content => " | About")
     end
