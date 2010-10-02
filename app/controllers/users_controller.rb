fc3af0055d2516bf78cf10c1ec8959813d8cb435
Phil Rand <philrand@gmail.com>
2010-10-01 23:05:30 -0700

    User model with passwords, and user show page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @title = @user.name # Rails 3 escapes this automatically.
   end
   def new
     @title = "Sign up"
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,20 @@
 #  created_at :datetime
 #  updated_at :datetime
 #
+require 'digest'
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  
+  # :password and :password_confirmation are virtual attributes,
+  # not stored in the database.  :password is created by the addr_accessor
+  # keyword, while :password_confirmation is created by the "validates 
+  # :password, :confirmation => true" declaration.
+  #
+  # (Would "attr_accessible .. :password_confirmation" have created the 
+  # virtual attribute by itself?  Or as the tutorial implies, is the validates
+  # declaration needed for that?)
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -20,4 +31,37 @@ class User < ActiveRecord::Base
   validates :email, :presence   => true,
                     :format     => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => { :within => 6..40 }
+  before_save :encrypt_password
+  
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  # Class method:  Find user by email; check password.
+  # Return nil if missing or batch match, otherwise return the user object.
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private
+    def encrypt_password
+      self.salt = make_salt if new_record?
+      self.encrypted_password = encrypt(password)
+    end
+    def encrypt(string)
+      secure_hash("#{salt}--#{string}")
+    end
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{password}")
+    end
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
